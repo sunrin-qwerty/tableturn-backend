8d1de14396d138dfca4cd8daf9300c4c27cc141a
Yuchan Han <yuchanhan71@gmail.com>
2025-08-29 08:37:45 +0900

    Add UUID and key generation functions to generate module

---
+++ app/common/utils/generate.py
@@ -0,0 +1,11 @@
+from uuid import uuid4
+from os import urandom
+
+__all__ = ["generate_uuid", "generate_key"]
+
+
+def generate_uuid() -> str:
+    return str(uuid4())
+
+def generate_key(length: int = 32) -> str:
+    return urandom(length).hex()
