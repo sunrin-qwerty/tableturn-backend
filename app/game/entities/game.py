f930f03f4694bbe1a43a4a61e260af09bc4e7aee
Yuchan Han <yuchanhan71@gmail.com>
2025-08-31 16:55:57 +0900

    ˆAdd Pydantic models for GameEntity and rename image_id to image_path

--- app/game/entities/game.py
+++ app/game/entities/game.py
@@ -3,14 +3,16 @@ from tortoise.models import Model
 from tortoise.contrib.postgres.fields import ArrayField
 
 from app.common.entity_mixins import UUIDEntityMixin
+from tortoise.contrib.pydantic import pydantic_model_creator
 
-__all__ = ["GameEntity"]
+
+__all__ = ["GameEntity", "GameEntity_Pydantic", "GameEntityCreate_Pydantic"]
 
 
 class GameEntity(Model, UUIDEntityMixin):
     name = fields.CharField(max_length=100)
     description = fields.TextField(null=True)
-    image_id = fields.CharField(max_length=100, null=True)
+    image_path = fields.CharField(max_length=100, null=True)
     theme = ArrayField(element_type="text", null=True)
     min_player_count = fields.IntField(default=1)
     max_player_count = fields.IntField(default=1)
@@ -21,3 +23,12 @@ class GameEntity(Model, UUIDEntityMixin):
 
     class Meta:
         table = "games"
+
+
+GameEntity_Pydantic = pydantic_model_creator(GameEntity, name="GameEntity")
+GameEntityCreate_Pydantic = pydantic_model_creator(
+    GameEntity,
+    name="GameEntityCreate",
+    exclude_readonly=True,
+    exclude=("id", "image_path"),
+)
