6e622d093ec451325131673284fabd5c2a2fac52
Yuchan Han <yuchanhan71@gmail.com>
2025-08-31 14:27:16 +0900

    Add GameContainer for dependency injection of GameRepository and GameService

---
+++ app/game/containers.py
@@ -0,0 +1,12 @@
+from dependency_injector import containers, providers
+
+from app.game.repository import GameRepository
+from app.game.services import GameService
+
+
+class GameContainer(containers.DeclarativeContainer):
+    repository: GameRepository = providers.Singleton(GameRepository)
+    service: GameService = providers.Singleton(
+        GameRepository,
+        game_repository=repository,
+    )
