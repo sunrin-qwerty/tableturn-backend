05ba624ccacf265ff0d8ec51b8d4071bd28c9968
Yuchan Han <yuchanhan71@gmail.com>
2025-09-04 14:20:07 +0900

    Add ROOM_ID and GAME_ID to typevar exports in __init__.py

--- app/common/types/__init__.py
+++ app/common/types/__init__.py
@@ -1,3 +1,10 @@
-from app.common.types.typevar import USER_ID, ACCESS_TOKEN, SESSION_ID, KIOSK_ID
+from app.common.types.typevar import (
+    USER_ID,
+    ACCESS_TOKEN,
+    SESSION_ID,
+    KIOSK_ID,
+    ROOM_ID,
+    GAME_ID,
+)
 
-__all__ = ["USER_ID", "ACCESS_TOKEN", "SESSION_ID", "KIOSK_ID"]
+__all__ = ["USER_ID", "ACCESS_TOKEN", "SESSION_ID", "KIOSK_ID", "ROOM_ID", "GAME_ID"]
